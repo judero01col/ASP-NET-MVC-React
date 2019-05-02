058505c0c4435d5ce6f48f5d885063849eeac818
Jurgen De Leon <jleon@nimocom.com>
2019-05-02 14:41:37 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace FirstReactApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Scripts/react/FirstReactApp.jsx
@@ -0,0 +1,16 @@
+﻿class HelloWorld extends React.Component {   
+    render() {
+        console.log("Hello World React JS! from react");
+        return (            
+            <div>
+                <h1>Hello World React JS!</h1>              
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('myContainer'));
+
+
